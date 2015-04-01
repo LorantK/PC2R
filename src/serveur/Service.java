f3ce3c7e226178892537660400ac6645410fa22e
LorantK <kevin.lorant@gmail.com>
2015-04-01 21:50:44 +0200

    Modification Serveur

--- src/serveur/Service.java
+++ src/serveur/Service.java
@@ -46,72 +46,83 @@ public class Service extends Thread {
 
 	@Override
 	public void run() {
+		String commande;
+		String[] param;
 		try {
 			while (true) {
-				String commande = in.readLine();
-				System.out.println(commande);
-				String[] param = commande.split("/");
-				switch (param[0]) {
-				case "CONNECT":
-
-					System.out.println("Nouvelle connexion");
-					start = true;
-					nomClient = param[1];
-					
-					out.println("WELCOME/" + nomClient);
-					out.flush();
-					out.println("AUDIO_PORT/2015/");
-					out.flush();
-
-					Canalaudio = serv.getcAudio().accept();
-					ServiceAudio s = new ServiceAudio(Canalaudio, nomClient, serv);
-
-					out.println("AUDIO_OK");
-					out.flush();
-
-					for (int i = 0; i < serv.out.size(); i++) {
-						serv.out.get(i).println("CONNECTED/" + nomClient + "/");
-						serv.out.get(i).flush();
-					}
-
-					synchronized (this) {
-						if (serv.getJam().getNbConnecte() < serv.getJam()
-								.getMax()) {
-							serv.getJam().addConnecte();
-							if (serv.getJam().getNbConnecte() == 1) {
-
-								out.println("EMPTY_SESSION");
-								out.flush();
-
-								while(true){
-									commande = in.readLine();
-									param = commande.split("/");
-									if(param[0].equals("SET_OPTIONS") && param.length == 3){
-										break;
-									}
-									out.println("Commande Invalide. Retapez une commande pour paramètrer la JAM");
-									out.flush();
-								}
-								serv.getJam().setStyle(param[1]);
-								serv.getJam().setTempo(param[2]);
-
-								out.println("ACK_OPTS");
-								out.flush();
-							} else {
-								out.println("CURRENT_SESSION/"
-										+ serv.getJam().getStyle() + "/"
-										+ serv.getJam().getTempo() + "/"
-										+ serv.getJam().getNbConnecte() + "/");
-							}
-						} else {
-							out.println("FULL_SESSION");
-							out.flush();
-
-							fullSession = true;
-						}
-					}
-
+				commande = in.readLine();
+				param = commande.split("/");
+				if(param[0].equals("CONNECT")){
 					break;
+				}
+				out.println("Commande Invalide. Retapez une commande pour vous connecter");
+				out.flush();
+			}
+
+			System.out.println("Nouvelle connexion");
+			start = true;
+			nomClient = param[1];
+
+			out.println("WELCOME/" + nomClient);
+			out.flush();
+			out.println("AUDIO_PORT/2015/");
+			out.flush();
+
+			Canalaudio = serv.getcAudio().accept();
+			ServiceAudio s = new ServiceAudio(Canalaudio, nomClient, serv);
+
+			out.println("AUDIO_OK");
+			out.flush();
+
+			for (int i = 0; i < serv.out.size(); i++) {
+				serv.out.get(i).println("CONNECTED/" + nomClient + "/");
+				serv.out.get(i).flush();
+			}
+
+			synchronized (this) {
+				if (serv.getJam().getNbConnecte() < serv.getJam()
+						.getMax()) {
+					serv.getJam().addConnecte();
+					if (serv.getJam().getNbConnecte() == 1) {
+
+						out.println("EMPTY_SESSION");
+						out.flush();
+
+						while(true){
+							commande = in.readLine();
+							param = commande.split("/");
+							if(param[0].equals("SET_OPTIONS") && param.length == 3){
+								break;
+							}
+							out.println("Commande Invalide. Retapez une commande pour paramètrer la JAM");
+							out.flush();
+						}
+						serv.getJam().setStyle(param[1]);
+						serv.getJam().setTempo(param[2]);
+
+						out.println("ACK_OPTS");
+						out.flush();
+					} else {
+						out.println("CURRENT_SESSION/"
+								+ serv.getJam().getStyle() + "/"
+								+ serv.getJam().getTempo() + "/"
+								+ serv.getJam().getNbConnecte() + "/");
+					}
+				} else {
+					out.println("FULL_SESSION");
+					out.flush();
+
+					fullSession = true;
+				}
+			}
+
+			while(true){
+
+				commande = in.readLine();
+				System.out.println(commande);
+				param = commande.split("/");
+				switch (param[0]) {
+
 				case "EXIT":
 					if (start) {
 						for (int i = 0; i < serv.out.size() - 1; i++) {
