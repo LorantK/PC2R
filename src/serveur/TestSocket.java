2473ba2aa6812685be1f6887934fc991bd3ba098
Ericola <eric-lim@hotmail.fr>
2015-04-02 20:59:23 +0200

    Modif Constructeur du serveur.

--- src/serveur/TestSocket.java
+++ src/serveur/TestSocket.java
@@ -6,7 +6,7 @@ public class TestSocket {
 
 	public static void main(String[] args) {
 		try {
-			Server s = new Server();
+			Server s = new Server(4, 1000, 2015);
 			s.start();
 		} catch (IOException e) {
 			System.err.println("Problème dans le serveur");
